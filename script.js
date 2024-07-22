33f45cd31c238494acc564c0a809af5f007345a2
Luka Goginashvili <145713150+blodwenlixiss@users.noreply.github.com>
2024-07-22 17:53:39 +0400

    fixed all the bugs optimized js code for slider, added header-responsive menu, added mobile slider

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-const sliderContainer = document.querySelector(".slider-container");
-const sliderBlocks = document.querySelectorAll(".slider-block");
-const nextBtn = document.querySelector(".next-btn");
-const prevBtn = document.querySelector(".prev-btn");
+const scrollContainer = document.querySelector(".slider-container");
+const scrollLeftButton = document.getElementById("prev-btn");
+const scrollRightButton = document.getElementById("next-btn");
+const sliderBlock = document.querySelectorAll(".slider-block");
 
 const bottomSliderContainer = document.querySelector(
   ".bottom-slider-container"
@@ -10,48 +10,65 @@ const rewardSlider = document.querySelectorAll(".reward-slider");
 const bottomNextBtn = document.querySelector(".next-btn-bottom");
 const bottomPrevBtn = document.querySelector(".prev-btn-bottom");
 
-let currentIndex = 0;
-let sliderBlockSize = sliderBlocks[0].offsetWidth;
-let bottomSliderBlockSize = rewardSlider[0].offsetWidth;
+const bottomSliderBlockSize = rewardSlider[0].offsetWidth;
 
-function handleSlider(container, size) {
-  container.style.transform = `translateX(-${currentIndex * (size + 19)}px)`;
-}
-console.log(sliderBlockSize);
-
-nextBtn.addEventListener("click", () => {
-  if (currentIndex < sliderBlocks.length - 3) {
-    currentIndex++;
-    handleSlider(sliderContainer, sliderBlockSize);
-  }
-});
-prevBtn.addEventListener("click", () => {
-  if (currentIndex > 0) {
-    currentIndex--;
-    handleSlider(sliderContainer, sliderBlockSize);
-  }
-});
+const sliderBlockSize = sliderBlock[0].offsetWidth;
 
 bottomNextBtn.addEventListener("click", () => {
-  if (currentIndex < rewardSlider.length - 3) {
-    currentIndex++;
-    handleSlider(bottomSliderContainer, bottomSliderBlockSize);
-  }
+  bottomSliderContainer.scrollBy({
+    left: bottomSliderBlockSize + 15,
+    behavior: "smooth",
+  });
 });
 
 bottomPrevBtn.addEventListener("click", () => {
-  if (currentIndex > 0) {
-    currentIndex--;
-    handleSlider(bottomSliderContainer, bottomSliderBlockSize);
-  }
+  bottomSliderContainer.scrollBy({
+    left: -bottomSliderBlockSize - 15,
+    behavior: "smooth",
+  });
+});
+scrollLeftButton.addEventListener("click", () => {
+  scrollContainer.scrollBy({ left: -sliderBlockSize - 15, behavior: "smooth" });
 });
 
-function myFunction(classList, arrowClass) {
+scrollRightButton.addEventListener("click", () => {
+  scrollContainer.scrollBy({ left: sliderBlockSize + 15, behavior: "smooth" });
+});
+
+function dropDown(classList, arrowClass) {
   const dropDownButton = document.getElementById(classList);
   dropDownButton.classList.toggle("show");
   if (dropDownButton.classList.contains("show")) {
     document.getElementById(arrowClass).style.rotate = "-90deg";
   } else {
     document.getElementById(arrowClass).style.rotate = "+90deg";
+  }
+}
+
+function dropdDownHeader(classList) {
+  const dropDownBtn = document.getElementById(classList);
+  if (!dropDownBtn.classList.contains("hidden-responsive")) {
+    document
+      .querySelector(".padding-main")
+      .classList.remove("hidden-responsive");
+    dropDownBtn.classList.toggle("hidden-responsive");
+    document
+      .querySelector(".responsive-policy")
+      .classList.add("hidden-responsive");
+    document
+      .querySelector(".responsive-policy-container")
+      .classList.add("hidden-responsive");
+  } else {
+    document.querySelector(".padding-main").classList.add("hidden-responsive");
+    document.getElementById("header-arrow").classList.remove("hidden");
+    document.getElementById("header-arrow-1").classList.remove("hidden");
+    document.getElementById("header-arrow-2").classList.remove("hidden");
+    document
+      .querySelector(".responsive-policy")
+      .classList.remove("hidden-responsive");
+    document
+      .querySelector(".responsive-policy-container")
+      .classList.remove("hidden-responsive");
+    dropDownBtn.classList.toggle("hidden-responsive");
   }
 }
